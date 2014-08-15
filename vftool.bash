01f29871bd8cc7de9042761a44eba6b3f2f70306
Crag Wolfe <cwolfe@redhat.com>
2014-08-15 17:14:38 -0400

    "-t -t" is better than "-t"

--- vftool.bash
+++ vftool.bash
@@ -1041,7 +1041,7 @@ run() {
     exit 1
   fi
   for domname in $vmset; do
-    ssh -t -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" root@$domname "$@"&
+    ssh -t -t -o "UserKnownHostsFile /dev/null" -o "StrictHostKeyChecking no" root@$domname "$@"&
   done
   wait
 }
